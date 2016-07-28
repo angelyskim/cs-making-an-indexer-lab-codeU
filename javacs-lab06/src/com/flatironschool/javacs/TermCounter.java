edff1870bbf93e4674d7cca39cad96e00e46c2cc
Young Sun Kim <youngsunkim@berkeley.edu>
2016-07-28 00:07:04 -0700

    Done.

--- javacs-lab06/src/com/flatironschool/javacs/TermCounter.java
+++ javacs-lab06/src/com/flatironschool/javacs/TermCounter.java
@@ -37,7 +37,13 @@ public class TermCounter {
 	 */
 	public int size() {
         // TODO: fill this in.
-		return -1;
+        int size = 0;
+        
+		for (int value : map.values()) {
+			size += value;
+		}
+
+		return size;
 	}
 
 	/**
